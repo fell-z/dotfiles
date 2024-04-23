8552f0c8727583ab41136afa7685a8833ea8c9ef
fell-z <matheusfelipeagapito@hotmail.com>
2024-04-23 01:05:54 -0300

    Add separate files for autocmds in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,45 +28,4 @@ require "ui"
 require "lsp"
 require "autopairs"
 require "greeter"
-
-local langs = {
-  c = {
-    augroup = vim.api.nvim_create_augroup("C", { clear = true }),
-    pattern = { "*.c", "*.h" },
-    config = function ()
-      vim.opt.shiftwidth = 4
-    end,
-  },
-  cpp = {
-    augroup = vim.api.nvim_create_augroup("Cpp", { clear = true }),
-    pattern = { "*.cpp", "*.hpp" },
-    config = function ()
-      vim.opt.shiftwidth = 4
-    end,
-  },
-  python = {
-    augroup = vim.api.nvim_create_augroup("Python", { clear = true }),
-    pattern = { "*.py", "*.pyw" },
-    config = function ()
-      vim.opt.shiftwidth = 4
-    end,
-  },
-}
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = langs.c.pattern,
-  group = langs.c.augroup,
-  callback = langs.c.config,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = langs.cpp.pattern,
-  group = langs.cpp.augroup,
-  callback = langs.cpp.config,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = langs.python.pattern,
-  group = langs.python.augroup,
-  callback = langs.python.config,
-})
+require "autocmds"
---
+++ nvim/lua/autocmds.lua
@@ -0,0 +1,43 @@
+local vim = vim
+
+local langs = {
+  c = {
+    augroup = vim.api.nvim_create_augroup("C", { clear = true }),
+    pattern = { "*.c", "*.h" },
+    config = function ()
+      vim.opt.shiftwidth = 4
+    end,
+  },
+  cpp = {
+    augroup = vim.api.nvim_create_augroup("Cpp", { clear = true }),
+    pattern = { "*.cpp", "*.hpp" },
+    config = function ()
+      vim.opt.shiftwidth = 4
+    end,
+  },
+  python = {
+    augroup = vim.api.nvim_create_augroup("Python", { clear = true }),
+    pattern = { "*.py", "*.pyw" },
+    config = function ()
+      vim.opt.shiftwidth = 4
+    end,
+  },
+}
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = langs.c.pattern,
+  group = langs.c.augroup,
+  callback = langs.c.config,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = langs.cpp.pattern,
+  group = langs.cpp.augroup,
+  callback = langs.cpp.config,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = langs.python.pattern,
+  group = langs.python.augroup,
+  callback = langs.python.config,
+})
