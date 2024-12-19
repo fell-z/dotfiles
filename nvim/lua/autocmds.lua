b3232d5f55dbbbb44371d6eebd9aa1f8faffa1c9
fell-z <matheusfelipeagapito@hotmail.com>
2024-12-19 11:18:06 -0300

    nvim: Refactor similar augroups into a default one

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,41 +1,28 @@
 local langs = {
-  c = {
-    augroup = vim.api.nvim_create_augroup("C", { clear = true }),
-    pattern = { "*.c", "*.h" },
-    config = function()
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.bo[bufnr].shiftwidth = 4
-    end,
-  },
-  cpp = {
-    augroup = vim.api.nvim_create_augroup("Cpp", { clear = true }),
-    pattern = { "*.cpp", "*.hpp" },
-    config = function()
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.bo[bufnr].shiftwidth = 4
-    end,
-  },
   python = {
-    augroup = vim.api.nvim_create_augroup("Python", { clear = true }),
+    group = vim.api.nvim_create_augroup("Python", { clear = true }),
     pattern = { "*.py", "*.pyw" },
-    config = function()
+    callback = function()
       local bufnr = vim.api.nvim_get_current_buf()
       vim.bo[bufnr].shiftwidth = 4
+      vim.bo[bufnr].tabstop = 4
     end,
   },
-  ruby = {
-    augroup = vim.api.nvim_create_augroup("Ruby", { clear = true }),
-    pattern = { "*.rb" },
-    config = function()
-      require("nvim-highlight-colors").turnOff()
-    end,
-  },
+  default = {
+    group = vim.api.nvim_create_augroup("Default", { clear = true }),
+    pattern = { "*" },
+    callback = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.bo[bufnr].shiftwidth = 2
+      vim.bo[bufnr].tabstop = 2
+    end
+  }
 }
 
 for _lang, settings in pairs(langs) do
   vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = settings.group,
     pattern = settings.pattern,
-    group = settings.augroup,
-    callback = settings.config,
+    callback = settings.callback,
   })
 end
