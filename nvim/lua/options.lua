831ecdf21ef62e244c5a7c2328733c09b7bf191c
fell-z <matheusfelipeagapito@hotmail.com>
2024-12-19 11:17:05 -0300

    nvim: Use vim.opt instead of vim.o

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,33 +1,25 @@
-local vim = vim
-
 if vim.fn.has("termguicolors") == 1 then
   vim.opt.termguicolors = true
 end
 
-vim.g.ruby_operators = 1
-vim.g.ruby_pseudo_operators = 1
-
 vim.opt.completeopt = { "menu", "menuone", "preview", "noselect" }
-
-vim.o.foldenable = true
-vim.o.foldmethod = "manual"
-
-vim.o.textwidth = 100
-vim.o.wrap = true
-
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
-
-vim.o.scrolloff = 12
-
-vim.o.numberwidth = 2
-vim.o.signcolumn = "yes"
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.cursorline = true
-vim.o.showmode = true
-vim.o.title = true
-
-vim.o.guicursor =
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.foldenable = true
+vim.opt.foldmethod = "manual"
+vim.opt.guicursor =
   "n-c-i-ci-r-cr-o:hor10,v-ve:block,a:blinkwait700-blinkoff400-blinkon250-Cursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.opt.number = true
+vim.opt.numberwidth = 2
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 12
+vim.opt.shiftwidth = 2
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.textwidth = 100
+vim.opt.title = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.wrap = true
