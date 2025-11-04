149dc817919b8fee4115cbc2d05c64b2c741db3a
fell-z <matheusfelipeagapito@hotmail.com>
2025-11-04 15:15:15 -0300

    nvim: Add utility to check if running in wsl

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -8,4 +8,20 @@ M.set_theme = function()
   end)
 end
 
+M.wsl_running = function()
+	local wslinfo_path = vim.fn.exepath("wslinfo")
+
+	if wslinfo_path == "" then
+		return false
+	end
+
+	if vim.fn.getftype(wslinfo_path) == "link" then
+		local exec = require("lazy.manage.process").exec
+
+		local _, code = exec("wslinfo", { args = { "--version" } })
+
+		return code == 0
+	end
+end
+
 return M
