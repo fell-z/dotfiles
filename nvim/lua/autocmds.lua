289dfdab8aae681cd5ab635beda734b5b3dd3bb4
fell-z <matheusfelipeagapito@hotmail.com>
2026-04-18 09:43:42 -0300

    nvim: refactor autocmd loading to add default values
    Added spellcheck via autocmd.
    Added terminal autocmd to disable spellcheck.

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,14 +1,14 @@
-local langs = {
-  c = {
-    group = vim.api.nvim_create_augroup("C", { clear = true }),
+local cmds = {
+  {
+    name = "C",
     pattern = { "*.c", "*.h" },
     callback = function()
       local bufnr = vim.api.nvim_get_current_buf()
       vim.bo[bufnr].path = vim.o.path .. "include,"
     end,
   },
-  python = {
-    group = vim.api.nvim_create_augroup("Python", { clear = true }),
+  {
+    name = "Python",
     pattern = { "*.py", "*.pyw" },
     callback = function()
       local bufnr = vim.api.nvim_get_current_buf()
@@ -16,31 +16,47 @@ local langs = {
       vim.bo[bufnr].tabstop = 4
     end,
   },
-  markdown = {
-    group = vim.api.nvim_create_augroup("Markdown", { clear = true }),
+  {
+    name = "Markdown",
     pattern = { "*.md" },
     callback = function()
       local bufnr = vim.api.nvim_get_current_buf()
       local winid = vim.api.nvim_get_current_win()
       vim.bo[bufnr].textwidth = 0
       vim.wo[winid].wrap = false
+    end,
+  },
+  {
+    name = "Terminal",
+    events = { "TermOpen" },
+    callback = function()
+      vim.opt_local.spell = false
     end
   },
-  default = {
-    group = vim.api.nvim_create_augroup("Default", { clear = true }),
-    pattern = { "*" },
+  {
+    name = "Spellcheck",
+    events = { "BufReadPost", "BufNewFile" },
+    callback = function()
+      vim.opt_local.spell = true
+      vim.opt_local.spelllang = { "en_us", "pt_br" }
+      vim.opt_local.spelloptions = "camel"
+      vim.opt_local.spellcapcheck = ""
+    end
+  },
+  {
+    name = "Default",
     callback = function()
       local bufnr = vim.api.nvim_get_current_buf()
       vim.bo[bufnr].shiftwidth = 2
       vim.bo[bufnr].tabstop = 2
-    end
-  }
+    end,
+  },
 }
 
-for _lang, settings in pairs(langs) do
-  vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    group = settings.group,
-    pattern = settings.pattern,
-    callback = settings.callback,
+for _, cmd in ipairs(cmds) do
+  vim.api.nvim_create_autocmd(cmd.events or { "BufEnter" }, {
+    group = vim.api.nvim_create_augroup(cmd.name, { clear = true }),
+    pattern = cmd.pattern or { "*" },
+    callback = cmd.callback,
   })
 end
