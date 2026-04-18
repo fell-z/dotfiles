9e6dce5c2a690b44f99b9fd2af8f366c0aebf45d
fell-z <matheusfelipeagapito@hotmail.com>
2026-04-18 09:18:43 -0300

    nvim: remove old/unused plugins and add missing dependency for neotest

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -1,6 +1,4 @@
 return {
-  { "nvim-lua/plenary.nvim", lazy = true },
-  { "nvim-lua/popup.nvim", lazy = true },
   { "IogaMaster/neocord", event = "VeryLazy", opts = {} },
 
   {
@@ -16,6 +14,8 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
       "olimorris/neotest-rspec",
     },
     config = function()
@@ -106,28 +106,5 @@ return {
   {
     "tpope/vim-dispatch",
     event = "VeryLazy",
-  },
-
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
   },
 }
